4938022c950df7f2e6673736e8b5a8d0180b0961
kqhome <a11dc094@dhw.ac.jp>
2019-11-10 19:04:58 +0900

    moved test

--- __tests__/mainTest.js
+++ __tests__/mainTest.js
@@ -1,6 +1,27 @@
-const main=require('../main');
+const main = require("../main");
+const sqlcon = require("../sqlcon");
 
+jest.mock("../sqlcon", () => ({
+  getRecordsInRange: () => {
+    return "r10";
+  },
+  getRecordsById:(id)=>{
+    return "r20";
+  },
+  getRecordsByUser:()=>{
+    return "r30";
+  },
+  getRecordsByStatus:()=>{
+    return "r40"
+  },
+  getSucceededRecords:()=>{
+    return "r50";
+  },
+  getFailedRecords:()=>{
+    return "r60";
+  },
+}));
 
-test('main case',()=>{
-  expect(main()).toBe(0);
-})
+test("main case", () => {
+  expect(main()).toBe("r1,r2,r3,r4,r5");
+});
