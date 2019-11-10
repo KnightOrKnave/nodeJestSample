3221b4536fa3c9e13b639180466c46c50a2a1460
kqhome <a11dc094@dhw.ac.jp>
2019-11-10 16:53:10 +0900

    add functions

---
+++ main.js
@@ -0,0 +1,27 @@
+const sqlcon=require('./sqlcon');
+
+
+
+
+function main(){
+  let fromDate=Date.now();
+  let toDate=Date.now();
+  let id="a0s98df7a908wy";
+  let statuses=["ok"];
+
+  //get from ~ to time duration recodes
+  let r1=sqlcon.getRecordsInRange(fromDate,toDate)//DateTime,DateTime
+  let r2=sqlcon.getRecordsById(id);//11304030
+  let r3=sqlcon.getRecordsByUser(id); //adijoe90403
+  let r4=sqlcon.getRecordsByStatus(statuses);//["ready","waiting","execution","succeeded","failed"]
+  let r5=sqlcon.getSucceededRecords();
+  let r6=sqlcon.getFailedRecords();
+
+
+  console.log(`${r1},${r2},${r3},${r4},${r5}`);
+  return 0;
+}
+
+main();
+
+module.exports=main;
---
+++ sqlcon.js
@@ -0,0 +1,25 @@
+var squel=require('squel');
+
+exports.getRecordsInRange=function(fromDate,toDate){
+  console.log("r1")
+}
+
+exports.getRecordsById=function (id){
+  console.log("r2")
+}
+
+exports.getRecordsByUser=function (id){
+  console.log("r3")
+}
+
+exports.getRecordsByStatus=function (statuses){
+  console.log("r4")
+}
+
+exports.getSucceededRecords=function (){
+  console.log("r5")
+}
+
+exports.getFailedRecords=function (){
+  console.log("r5")
+}
